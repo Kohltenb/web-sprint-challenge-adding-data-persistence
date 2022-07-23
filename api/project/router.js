5cb60257d6a88ce2592b6b0a2961113ca4d02c15
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-22 18:23:59 -0600

    Routers connected to server

--- api/project/router.js
+++ api/project/router.js
@@ -8,7 +8,7 @@ router.get('/', (req, res, next) => {
 
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(500).json({
-        customMessage: 'Something is broken in router KOHLTEN',
+        customMessage: 'Something is broken in PROJECT router KOHLTEN',
         message: err.message
     })
 })
